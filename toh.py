dc7702bc1ee15b86dcb94118da98f98a5ae59344
Kevin LaBranche <klabran@htomail.com>
2022-06-02 20:41:27 -0700

    prep for no print

--- toh.py
+++ toh.py
@@ -1,12 +1,14 @@
 # https://code.visualstudio.com/docs/python/python-tutorial
 
-def toh(discCount, source_rod, destination_rod, placeholder_rod):           
+def toh(steps,discCount, source_rod, destination_rod, placeholder_rod):           
     if discCount == 1:
         print("Move disc 1 from rod",source_rod,"to rod",destination_rod)
+        steps.append((1,source_rod,destination_rod))
         return
-    toh(discCount-1, source_rod, placeholder_rod, destination_rod)
+    toh(steps,discCount-1, source_rod, placeholder_rod, destination_rod)
     print("Move disc",discCount,"from rod",source_rod,"to rod",destination_rod)
-    toh(discCount-1, placeholder_rod, destination_rod, source_rod)
+    steps.append((discCount,source_rod,destination_rod))
+    toh(steps,discCount-1, placeholder_rod, destination_rod, source_rod)
 
 def main():
     try:
@@ -18,4 +20,6 @@ def main():
 
     print()
     print("Moving",discCount,"discs from rod A to rod C", end="\n\n")
-    toh(discCount, 'A', 'C', 'B')
+    steps=[]
+    toh(steps,discCount, 'A', 'C', 'B')
+    print(steps)
