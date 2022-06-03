f784697a9b310561f15b5b59c84bc0440e2644ad
Kevin LaBranche <klabran@htomail.com>
2022-06-02 20:17:03 -0700

    intro flask

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+from toh import toh
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+@app.route("/toh/<int:discCount>")
+def toh_solved(discCount):
+    print('Request for index page received')
+    output = toh(discCount,'A', 'C', 'B')
+    return render_template('index.html',count=discCount)
--- toh.py
+++ toh.py
@@ -1,4 +1,6 @@
-def toh(discCount , source_rod, destination_rod, placeholder_rod):           
+# https://code.visualstudio.com/docs/python/python-tutorial
+
+def toh(discCount, source_rod, destination_rod, placeholder_rod):           
     if discCount == 1:
         print("Move disc 1 from rod",source_rod,"to rod",destination_rod)
         return
@@ -6,14 +8,14 @@ def toh(discCount , source_rod, destination_rod, placeholder_rod):
     print("Move disc",discCount,"from rod",source_rod,"to rod",destination_rod)
     toh(discCount-1, placeholder_rod, destination_rod, source_rod)
 
-#discCount = 3
-try:
-    discCount = int(input("How many disks do you want to move?"))
-except ValueError:
-    print()
-    print("discCount is not a number... defaulting to 3")
-    discCount=3
+def main():
+    try:
+        discCount = int(input("How many disks do you want to move?"))
+    except ValueError:
+        print()
+        print("discCount is not a number... defaulting to 3")
+        discCount=3
 
-print()
-print("Moving",discCount,"discs from rod A to rod C", end="\n\n")
-toh(discCount, 'A', 'C', 'B')
+    print()
+    print("Moving",discCount,"discs from rod A to rod C", end="\n\n")
+    toh(discCount, 'A', 'C', 'B')
