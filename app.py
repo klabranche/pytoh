1a0807dc952c4ee140d5d7eeb892d9336110d8ec
Kevin LaBranche <klabran@hotmail.com>
2022-06-04 15:45:58 +0000

    updated again...

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 from toh import toh
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1>Home of the Tower Of Hanoi solver!</h1>"
+    """home page for app"""
+    return f"<h1>Home of the Tower Of Hanoi solver!</h1><p><a href={url_for('toh_solved',disc_count=3)}>Solve TOH for 3 discs!</a></p>"
 
-@app.route("/toh/<int:discCount>")
-def toh_solved(discCount):
-    print('Request for TOH page received')
+@app.route("/toh/<int:disc_count>")
+def toh_solved(disc_count):
+    """"page to solve toh based on passed in number of discs in the url"""
     steps = []
-    toh(steps,discCount,'A', 'C', 'B')
-    return render_template('index.html',count=discCount, results = steps)
+    toh(steps,disc_count,'A', 'C', 'B')
+    return render_template('index.html',count=disc_count, results = steps)
