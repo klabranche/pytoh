b060c0a528c337191ede37f9e363d399b7c109da
Kevin LaBranche <klabran@hotmail.com>
2022-06-07 01:38:25 +0000

    ran autopep8

--- console.py
+++ console.py
@@ -1,5 +1,6 @@
 from toh import toh
 
+
 def console():
     """call toh from console"""
     try:
@@ -8,15 +9,16 @@ def console():
     except ValueError:
         print()
         print("discCount is not a number... defaulting to 3")
-        discs_to_move=3
+        discs_to_move = 3
 
     print()
-    print("Moving",discs_to_move,"discs from rod A to rod C", end="\n\n")
-    steps=[]
-    toh(steps,discs_to_move, 'A', 'C', 'B')
+    print("Moving", discs_to_move, "discs from rod A to rod C", end="\n\n")
+    steps = []
+    toh(steps, discs_to_move, 'A', 'C', 'B')
     for __, step in enumerate(steps):
-        print("Move disc", step[0],"from rod",step[1],"to rod",step[2])
+        print("Move disc", step[0], "from rod", step[1], "to rod", step[2])
 
     print(steps)
+
 
 console()
